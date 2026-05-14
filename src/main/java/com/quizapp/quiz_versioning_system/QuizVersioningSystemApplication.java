f3ef2789e18a2782a16223fe7dea67e120989a26
Jayraj Gajul <jayraj.gajul@mindbowser.com>
2026-05-14 14:36:43 +0530

    intital commit

---
+++ src/main/java/com/quizapp/quiz_versioning_system/QuizVersioningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.quizapp.quiz_versioning_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizVersioningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizVersioningSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizapp/quiz_versioning_system/QuizVersioningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizapp.quiz_versioning_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizVersioningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
